d455aecc479365e47cda870ab7ff313f370a472d
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-08-12 15:03:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,3 @@
 class Song 
   attr_accessor :title, :artist
-
-def initialize(title)
-  @title = title
 end
-end
-
-class Artist 
-  attr_accessor :name 
-  
-  def initialize(name)
-    @name = name
-end
-end
-beyonce = Artist.new("Beyonce")
-song = Song.new("7/11")
-
-
-song.artist = "Beyonce"
